07b99d4d7fd84cb4854bd5bf875228c09366ab13
iklyleo <anniehello@naver.com>
2024-03-21 18:19:41 +0900

    0321학원에서
    index의틀과 menu 선택 부분 수정했습니다.

--- KYU/index/index.js
+++ KYU/index/index.js
@@ -1,37 +1,44 @@
-$(function(){
-    var mainSwiper = new Swiper('.swiper0', {
-        direction: 'vertical',
-        // speed: 1000,
-        mousewheel: true,
-        // scrollbar: hide
-    });
-
-    var mySwiper = new Swiper('.swiper1', {
-        direction: 'horizontal', // 수평 방향 슬라이드
-        loop: true, // 무한 루프
-        autoplay: {
-            delay: 3000 // 자동 재생 딜레이 시간 (ms)
-        },
-        speed: 500, // 슬라이드 애니메이션 속도 (ms)
-        
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // }
-    });
-
-    var mySwiper_2 = new Swiper('.swiper2', {
-        direction: 'horizontal', // 수평 방향 슬라이드
-        loop: true, // 무한 루프
-        autoplay: {
-            delay: 3000 // 자동 재생 딜레이 시간 (ms)
-        },
-        speed: 1000, // 슬라이드 애니메이션 속도 (ms)
-        
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // }
-    });
-
-});
+// test.js
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+  //   spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+  var swiper2 = new Swiper(".mySwiper2", {
+    direction: "horizontal",
+    effect: "fade",
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+  var swiper = new Swiper(".mySwiper3", {
+    effect: "fade",
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
